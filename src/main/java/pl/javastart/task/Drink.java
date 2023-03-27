bc915fee725996224ce49747eef4c75fe6b3b165
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-03-27 19:32:18 +0200

    Rozwiązanie

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,57 @@
+package pl.javastart.task;
+
+
+public class Drink {
+    private String name;
+    private double price;
+    boolean alcohol;
+
+
+    public String getWaterName() {
+        return "woda";
+
+    }
+
+
+    public int getWaterCapacity() {
+        return 100;
+    }
+
+
+    public String getLemonJuiceName() {
+        return "sok cytrynowy";
+    }
+
+
+    public int getLemonJuiceCapacity() {
+        return 40;
+    }
+
+
+    public String getVodkaName() {
+        return "wódka";
+    }
+
+
+    public int getVodkaCapacity() {
+        return 70;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,18 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Drink drink1 = new Drink();
+        drink1.setName("Mojito");
+        drink1.setPrice(13.0);
+        drink1.alcohol = true;
+        System.out.println("Drink: " + drink1.getName());
+        System.out.println("Cena: " + drink1.getPrice() + "zł");
+        System.out.println("Czy zawiera alkohol?" + " " + drink1.alcohol);
+        System.out.println("Składniki:");
+        System.out.println(drink1.getWaterName());
+        System.out.println(drink1.getLemonJuiceName());
+        System.out.println(drink1.getVodkaName());
+        int ingredientsCapacity = drink1.getLemonJuiceCapacity() + drink1.getWaterCapacity() + drink1.getVodkaCapacity();
+        System.out.println("Pojemność: " + ingredientsCapacity + "ml");
     }
 }
