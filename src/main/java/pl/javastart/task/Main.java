0166886abc5e0f4ecd38dba7ded1403715dabb70
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-03-28 23:11:15 +0200

    Poprawka

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,17 +3,30 @@ package pl.javastart.task;
 public class Main {
     public static void main(String[] args) {
         Drink drink1 = new Drink();
-        drink1.setName("Mojito");
-        drink1.setPrice(13.0);
+        drink1.name = "Mojito";
+        drink1.price = 13.0;
         drink1.alcohol = true;
-        System.out.println("Drink: " + drink1.getName());
-        System.out.println("Cena: " + drink1.getPrice() + "zł");
+        drink1.ingredient1 = new Ingredient();
+        drink1.ingredient1.name = "woda";
+        drink1.ingredient1.volume = 90;
+        drink1.ingredient2 = new Ingredient();
+        drink1.ingredient2.name = "sok cytrynowy";
+        drink1.ingredient2.volume = 60;
+        drink1.ingredient3 = new Ingredient();
+        drink1.ingredient3.name = "wódka";
+        drink1.ingredient3.volume = 60;
+
+        System.out.println("Drink: " + drink1.name);
+        System.out.println("Cena: " + drink1.price + "zł");
         System.out.println("Czy zawiera alkohol?" + " " + drink1.alcohol);
         System.out.println("Składniki:");
-        System.out.println(drink1.getWaterName());
-        System.out.println(drink1.getLemonJuiceName());
-        System.out.println(drink1.getVodkaName());
-        int ingredientsCapacity = drink1.getLemonJuiceCapacity() + drink1.getWaterCapacity() + drink1.getVodkaCapacity();
-        System.out.println("Pojemność: " + ingredientsCapacity + "ml");
+        String ingredient1Name = drink1.ingredient1.name;
+        String ingredient2Name = drink1.ingredient2.name;
+        String ingredient3Name = drink1.ingredient3.name;
+        System.out.println(ingredient1Name);
+        System.out.println(ingredient2Name);
+        System.out.println(ingredient3Name);
+
+        System.out.println("Pojemność: " + (drink1.ingredient1.volume + drink1.ingredient2.volume + drink1.ingredient3.volume) + "ml");
     }
 }
